4eafc543da5e642c827f12d90d6dbea06e40bbc7
Jn22L <peaceofjn@gmail.com>
2020-10-07 22:18:06 +0900

    Update and rename es6 to hello_es6.js
    ES6 연습

--- hello_es6.js
+++ hello_es6.js
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>ES6 Destructuring & 역따옴표 사용연습</h2>
+
+<p id="demo"></p>
+
 <script>
 
 const today = new Date(); // Tue May 21 2019 22:19:42 GMT+0900 (한국 표준시)
@@ -7,3 +15,6 @@ const [year, month, day] = formattedDate.split('-');
 document.getElementById("demo").innerHTML = `${year}/${month}/${day}`;
 
 </script>
+
+</body>
+</html>
